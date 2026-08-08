60cc43fb846bad0a722a125a6dada766a593368e
Sachin <sachin.sachin_cs25@gla.ac.in>
2026-08-08 21:42:05 +0530

    init commit

---
+++ src/Array_max_min.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Array_max_min{
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            int min = arr[0];
+            int max = arr[0];
+            for (int i = 1; i < n; i++) {
+                if (arr[i] > max) {
+                    max = arr[i];}
+                if (arr[i] < min) {
+                    min = arr[i];
+                }
+            }
+            System.out.println("Maximum = " + max);
+            System.out.println("Minimum = " + min);
+        }
+    }
+
